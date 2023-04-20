147c0bdd1cd301e31641bc933e67bf6c75b1379e
Milan Sony <milanfrom2001@gmail.com>
2023-04-20 21:22:13 +0530

    change window title name

--- test.py
+++ test.py
@@ -2,5 +2,9 @@ from tkinter import *
 
 # creating an object of class Tk()
 window = Tk()
+
+# change window title
+window.title("MilanSony")
+
 # mainloop is used to  run the window/tinker 
 window.mainloop()
