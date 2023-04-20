646bd731d5e2a0c637a829911949db8d4b2cc574
Milan Sony <milanfrom2001@gmail.com>
2023-04-20 21:31:39 +0530

    more comments added

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ window.geometry("600x400") # 600 = width & 400 = height
 
 # disable resizing of window
 window.resizable(False, False) #  1st False - width, 2nd False - height
+# if we give 1st parameter as True and 2nd as False we can adjust the width only, if we give 2nd parameter as True and 1st as False we can adjust the height only
 
 # mainloop is used to  run the window/tinker 
 window.mainloop()
