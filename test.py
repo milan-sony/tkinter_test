caa5d9c1f8cae7d4832f5aa218c2f0c2ad81c48c
Milan Sony <milanfrom2001@gmail.com>
2023-04-20 21:24:50 +0530

    giving fixed width and height

--- test.py
+++ test.py
@@ -6,5 +6,9 @@ window = Tk()
 # change window title
 window.title("MilanSony")
 
+# change/giving fixed size to window
+# 600 = width & 400 = height
+window.geometry("600x400")
+
 # mainloop is used to  run the window/tinker 
 window.mainloop()
